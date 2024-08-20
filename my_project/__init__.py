0fa55207a1171be5e0e463969ff7462ea5f32aa0
ilan-gold <ilanbassgold@gmail.com>
2024-08-20 17:15:36 -0400

    feat: another function

--- my_project/__init__.py
+++ my_project/__init__.py
@@ -1,3 +1,3 @@
-from my_func import my_func
+from my_func import my_func, my_other_func
 
-__all__ = ['my_func']
+__all__ = ['my_func', 'my_other_func']
--- my_project/my_func.py
+++ my_project/my_func.py
@@ -1,5 +1,8 @@
 def my_func():
     return internal_func()
 
+def my_other_func():
+    return 1
+
 def internal_func():
     return 2
